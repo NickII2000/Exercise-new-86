137a479a0f6ef77610a1cfe593e92c73613167b4
Nick <Nick_II2000@mail.ru>
2022-06-27 07:42:36 +0600

    2-th commit

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+console.log('Запрос данных...');
+
+setTimeout(() => {
+    console.log('Подготовка данных...');
+
+    const product = {
+        name: 'TV',
+        price: 2000,
+    };
+
+    setTimeout(() => {
+        product.status = 'order';
+        console.log(product);
+    }, 2000);
+
+}, 2000);
